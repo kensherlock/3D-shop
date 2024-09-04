7e4a7716248a2587c19ae19dc800024a0a4e4f92
KenWong <kenasdwasd@gmail.com>
2024-09-04 23:34:06 +0800

    Update carmera change

--- src/components/ShowText.jsx
+++ src/components/ShowText.jsx
@@ -8,41 +8,61 @@ function ShowText() {
 
   const controls = useRef();
 
-  const meshFitCarmeraStore = useRef();
+  const meshFitCarmeraHome = useRef();
+
+  const meshFitCarmeraGuy = useRef();
 
   const [currentPage, setCurrentPage] = useAtom(currentPageAtom);
 
   const intro = async () => {
     controls.current.dolly(-20);
     controls.current.smoothTime = 1.6;
-    controls.current.dolly(20, true);
     setTimeout(() => {
       setCurrentPage("home");
     }, 1200);
+    fitCamera();
+  }
+
+  const fitCamera = async () => {
+    if (currentPage === "guy") {
+      controls.current.fitToBox(meshFitCarmeraGuy.current, true);
+    }else if (currentPage === "home") {
+      controls.current.fitToBox(meshFitCarmeraHome.current, true);
+    }
   }
 
   useEffect(() => {
     intro();
   }, [])
 
+  useEffect(() => {
+    fitCamera();
+    window.addEventListener("resize", fitCamera);
+    return () => window.removeEventListener("resize", fitCamera);
+  }, [currentPage]);
+
   return (
     <>
       <CameraControls ref={controls} />
+      <mesh ref={meshFitCarmeraHome} position-y={-0.48} visible={false}>
+          <boxGeometry args={[2, 1, 2]} />
+          <meshBasicMaterial color='red' transparent opacity={0.5} />
+      </mesh>
       <Text
         font='fonts/Poppins-Black.ttf'
         position-z={1}
         lineHeight={0.8}
         textAlign="center"
-        fontSize={1}
+        fontSize={0.8}
         anchorY={"bottom"}>
         Guy
         <meshBasicMaterial color='red' />
       </Text>
       <group position={[0, -0.48, -20]}>
-        <Guy scale={0.1}  position={[0, -0.48, 0]} />
-        <mesh ref={meshFitCarmeraStore}>
+        <Guy scale={0.1}  position={[0, 0, 0]} />
+        <mesh ref={meshFitCarmeraGuy} visible={false} position-y={1}>
           <boxGeometry args={[2, 1, 2]} />
-          <meshBasicMaterial color='red' />
+          <meshBasicMaterial color='red' transparent opacity={0.5} />
         </mesh>
       </group>
       <mesh position-y={-0.48} rotation-x={-Math.PI / 2}>
